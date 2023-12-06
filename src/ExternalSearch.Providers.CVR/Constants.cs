2df0b672521c387e8b91181521266a5ea6b356cd
andrew-hardy <aha@cluedin.com>
2023-12-06 14:40:46 +1000

    feat: configurable types and keys (#8)
    feat: configurable types and keys
    
    ---------
    
    Co-authored-by: Ziggy <zygis0408@gmail.com>

--- src/ExternalSearch.Providers.CVR/Constants.cs
+++ src/ExternalSearch.Providers.CVR/Constants.cs
@@ -11,6 +11,15 @@ namespace CluedIn.ExternalSearch.Providers.CVR
         public const string ProviderName = "CVR";
         public static readonly Guid ProviderId = Core.Constants.ExternalSearchProviders.CVRId;
 
+        public struct KeyName
+        {
+            public const string AcceptedEntityType = "acceptedEntityType";
+            public const string OrgNameKey = "orgNameKey";
+            public const string CVRKey = "cvrKey";
+            public const string CountryKey = "countryKey";
+            public const string WebsiteKey = "websiteKey";
+        }
+
         public static string About { get; set; } = "CVR is the Danish state's master register of information about businesses.";
         public static string Icon { get; set; } = "Resources.logo.svg";
         public static string Domain { get; set; } = "https://datacvr.virk.dk/data";
@@ -18,6 +27,43 @@ namespace CluedIn.ExternalSearch.Providers.CVR
         public static AuthMethods AuthMethods { get; set; } = new AuthMethods
         {
             token = new List<Control>()
+            {
+                new Control()
+                {
+                    displayName = "Accepted Entity Type",
+                    type = "input",
+                    isRequired = false,
+                    name = KeyName.AcceptedEntityType
+                },
+                new Control()
+                {
+                    displayName = "Organization Name vocab key",
+                    type = "input",
+                    isRequired = false,
+                    name = KeyName.OrgNameKey
+                },
+                new Control()
+                {
+                    displayName = "CVR vocab key",
+                    type = "input",
+                    isRequired = false,
+                    name = KeyName.CVRKey
+                },
+                new Control()
+                {
+                    displayName = "Country vocab key",
+                    type = "input",
+                    isRequired = false,
+                    name = KeyName.CountryKey
+                },
+                new Control()
+                {
+                    displayName = "Website vocab key",
+                    type = "input",
+                    isRequired = false,
+                    name = KeyName.WebsiteKey
+                }
+            }
         };
 
         public static IEnumerable<Control> Properties { get; set; } = new List<Control>()
