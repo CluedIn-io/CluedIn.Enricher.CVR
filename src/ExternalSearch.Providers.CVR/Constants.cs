667e5d3651dcf2e60b15f84b667ed675d848eabb
Yi-Wei <86753236+ywei390@users.noreply.github.com>
2025-02-27 11:57:22 +0800

    feat: Update terminology (#25)

--- src/ExternalSearch.Providers.CVR/Constants.cs
+++ src/ExternalSearch.Providers.CVR/Constants.cs
@@ -19,7 +19,7 @@ namespace CluedIn.ExternalSearch.Providers.CVR
                     "type": "list-item",
                     "children": [
                       {
-                        "text": "Add the entity type to specify the golden records you want to enrich. Only golden records belonging to that entity type will be enriched."
+                        "text": "Add the business domain to specify the golden records you want to enrich. Only golden records belonging to that business domain will be enriched."
                       }
                     ]
                   },
@@ -57,11 +57,11 @@ namespace CluedIn.ExternalSearch.Providers.CVR
             {
                 new Control()
                 {
-                    DisplayName = "Accepted Entity Type",
+                    DisplayName = "Accepted Business Domain",
                     Type = "entityTypeSelector",
                     IsRequired = true,
                     Name = KeyName.AcceptedEntityType,
-                    Help = "The entity type that defines the golden records you want to enrich (e.g., /Organization)."
+                    Help = "The business domain that defines the golden records you want to enrich (e.g., /Organization)."
                 },
                 new Control()
                 {
